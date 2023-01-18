2f1b9a7ee0284d657e185f60c279d4218056b9dd
James Boland <jamespatrickboland@gmail.com>
2023-01-18 00:39:32 +0000

    (Hard-coded) Yaml file is now read to a Yml struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+
+extern crate yaml_rust;
+use yaml_rust::{YamlLoader};
+
 fn main() {
-    println!("Hello, world!");
+    let file_name: String = "ex.yml".to_string();
+    let yml: String = fs::read_to_string(file_name).expect("Should have been able to read the file");
+
+    let docs = YamlLoader::load_from_str(&yml).unwrap();
+    println!("{:?}", docs);
 }
